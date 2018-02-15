d596c867439f1f47008d58659f021231cd32b1f9
vanndhall <szymon.mantey@wp.pl>
2018-02-15 21:39:51 +0100

    rep2

---
+++ KosztyImprezy/DinnerParty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace KosztyImprezy
+{
+    class DinnerParty
+    {
+        public int NumberOfPeople { get; set; }
+        public int CostOfBeveragesPerPerson { get; set; }
+        public int CostOfDecorations { get; set; }
+
+
+        public void SetHealthyOption(bool lysy)
+        { }
+        public void CalculateCostOfDecoration(bool lolek)
+        { }
+        public void CalculateCost()
+        { }
+    }
+}
--- KosztyImprezy/Form1.cs
+++ KosztyImprezy/Form1.cs
@@ -12,9 +12,14 @@ namespace KosztyImprezy
 {
     public partial class Form1 : Form
     {
+        DinnerParty dinnerParty;
         public Form1()
         {
             InitializeComponent();
+            dinnerParty = new DinnerParty() { NumberOfPeople = 5 };
+            dinnerParty.SetHealthyOption(false);
+            dinnerParty.CalculateCostOfDecoration(true);
+            DisplayDinnerParty();
         }
     }
 }
